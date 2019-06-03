006447908b4604d2dd528eaca7f05fbb41284c86
vbuckner <vbuckner@gmail.com>
2019-06-03 22:38:05 +0200

    changed coding to attempt split with extra columns (fails)

--- app.py
+++ app.py
@@ -11,33 +11,28 @@ try:
     #xlsx = Excel_File('t.xlsx')
     #df = pd.read_excel(xlsx,0)
     print(df.head(3))
+    print(len(df))
 except Exception as err:
     print('Problem with reading the file.<br/> '+err)
 
-checkin = df[df['Transaction']=='Check-in']
-checkin = checkin[checkin['Date','Check-in','Departure']]
-checkin.rename({'Check-in':'Time'})
-checkin = checkin.reset_index()
-checkin['Arrival','Cost','Duration','IsWork']=['',0.0,0,'']
-#checkin
+checkin = df[df['Transaction']=='Check-in'].rename(columns={'Check-in':'Time'}).reset_index()
+checkin['Type','Arrival','Duration','Cost']=['Check-in','',0.,0.]
+#checkin.head(5)
 
 checkout = df[df['Transaction']=='Check-out']
-checkout = checkout[checkin['Date','Check-out','Destination','Cost']]
-checkout.rename({'Destination':'Arrival','Check-out':'Time'})
-checkout['Departure','Duration'
-#checkout
+checkout = checkout[['Date','Check-out','Destination','Amount']].rename(columns={'Check-out':'Time','Amount':'Cost'}).reset_index()
+checkout['Type','Departure','Duration']=['Check-out','',0.]
+#checkout.head(5)
 
 #cleaned = pd.Series([dt.date(2019,6,2),dt.time(22,7,0),'','',0.0,dt.time(0,0,0),'Y'])
 #from_dict(['Date','Time','Departure','Arrival','Cost','Duration','IsWork'])
 #.columns(['Date','Time','Departure','Arrival','Cost','Duration','IsWork'])
 #dt.date(2019,6,2)
 #print(str(dt.time(22,11,1)))
-##cleaned = pd.DataFrame(columns=['Date','Time','Departure','Arrival','Cost','Duration','IsWork'])
+cleaned = pd.DataFrame(columns=['Type','Date','Time','Departure','Arrival','Cost','Duration','IsWork'])
+#.astype('object','datetime64','datetime64','object','object','float_','float_','bool_')
 #cleaned
 
-#checkin[['Date','Check-in','Departure']]
-#cleaned.append(checkin['Date','Check-in','Departure'].rename({'Check-in':'Time'}))
-#cleaned.append([checkin,checkout],ignore_index)
-#cleaned
+cinout = checkin['Date','Time','Departure'].append(checkout['Date','Time','Arrival','Cost'])
+#cinout
 
-print('done')
